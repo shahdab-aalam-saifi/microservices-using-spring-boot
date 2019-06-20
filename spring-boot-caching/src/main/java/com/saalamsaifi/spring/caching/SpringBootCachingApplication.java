20cab1fe37e62e8101fce1fbf95f87b3be2f57ad
Shahdab Aalam Saifi <saalamsaifi@gmail.com>
2019-06-20 18:15:40 +0530

    Amended notes and new spring projects

---
+++ spring-boot-caching/src/main/java/com/saalamsaifi/spring/caching/SpringBootCachingApplication.java
@@ -0,0 +1,13 @@
+package com.saalamsaifi.spring.caching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCachingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCachingApplication.class, args);
+	}
+
+}

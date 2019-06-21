5f13b7e0579d4fc672372635d9574560eefa7375
Shahdab Aalam Saifi <saalamsaifi@gmail.com>
2019-06-21 18:06:37 +0530

    Spring security and microservices

---
+++ spring-boot-security-cors/src/main/java/com/saalamsaifi/spring/security/cors/SpringBootSecurityCorsApplication.java
@@ -0,0 +1,13 @@
+package com.saalamsaifi.spring.security.cors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityCorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityCorsApplication.class, args);
+	}
+
+}

0444488c09de53cdbeb9cd84bd8466c3bb5bfa0f
Shahdab Aalam Saifi <shahdabaalam.mohamedahamadsaifi@vodafone.com>
2020-02-21 17:00:29 +0530

    Authentication - LDAP

---
+++ spring-boot-authenticate-ldap/README.md
@@ -0,0 +1,8 @@
+# Authenticating a User with LDAP
+
+[Authenticating a User with LDAP][1]
+
+
+## References
+
+[1]: https://spring.io/guides/gs/authenticating-ldap/
---
+++ spring-boot-authenticate-ldap/src/main/java/com/saalamsaifi/spring/boot/authenticate/ldap/SpringBootAuthenticateLdapApplication.java
@@ -0,0 +1,13 @@
+package com.saalamsaifi.spring.boot.authenticate.ldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAuthenticateLdapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAuthenticateLdapApplication.class, args);
+	}
+
+}
